dc8766712f4c8daf31a25d16da4eb36a77b0d0ce
Matvey Vidyasov <kicksukoh@gmail.com>
2025-01-05 13:52:34 +0300

    chore: созданы папки для дочерних компонентов Main

---
+++ src/components/Main/PortfolioContainer/index.tsx
@@ -0,0 +1,11 @@
+import style from './PortfolioContainer.module.css';
+
+function PortfolioContainer() {
+    return (
+        <div className={style.yo}>
+            {/* Тут должен быть мап */}
+        </div>
+    );
+}
+
+export default PortfolioContainer;
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,17 @@
+
 import style from "./Main.module.css"
+import FirstContainer from "./FirstContainer";
+import AboutContainer from "./AboutContainer";
+
 
 const Main = () => {
     return (
         <main className={style.main}>
-            MAIN
+            <FirstContainer />
+            <AboutContainer />
+            {/* ServicesContainer */}
+            {/* PortfolioContainer */}
+            {/* FaqsContainer */}
         </main>
     );
 }
