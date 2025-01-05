3a0dcb3b5cc4fddb6a043bd902d715312de49ffc
Matvey Vidyasov <kicksukoh@gmail.com>
2025-01-05 21:20:09 +0300

    feat: сверстан компонент PortfolioContainer

--- src/components/Main/PortfolioContainer/index.tsx
+++ src/components/Main/PortfolioContainer/index.tsx
@@ -1,10 +1,44 @@
 import style from './PortfolioContainer.module.css';
+import Button from '../../Button';
 
 function PortfolioContainer() {
     return (
-        <div className={style.yo}>
-            {/* Тут должен быть мап */}
-        </div>
+        <section className={style.portfolioContainer}>
+
+            <div className={style.title}>
+                <div className={style.titleTextContent}>
+                    <h3>Portfolio</h3>
+                    <h2>Explore My photography work.</h2>
+                </div>
+
+                <div className={style.titleButtonsContainer}>
+                    <div className={style.turnButtonsContainer}>
+                        <Button variant="turn" customClass={style.prevButton}></Button>
+                        <Button variant="turn" customClass={style.nextButton}></Button>
+                    </div>
+                    <Button customClass={style.titleButton}>View All Works &rarr;</Button>
+                </div>
+
+                <div className={style.content}>
+
+                    {/* Заполнить по шаблону с помощью map */}
+                    <article className='contentItem'>
+                        <img src="" alt="" />
+                        <div className={style.infoContainer}>
+                            <div className={style.textInfo}>
+                                <h5>{}</h5>
+                                <p>{}</p>
+                            </div>
+                            <div className={style.linkInfo}>
+                                <a href="">View project</a>
+                                <img src="" alt="" />
+                            </div>
+                        </div>
+                    </article>
+
+                </div>
+            </div>
+        </section>
     );
 }
 
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,17 @@
-
 import style from "./Main.module.css"
 import FirstContainer from "./FirstContainer";
 import AboutContainer from "./AboutContainer";
-
+import ServicesContainer from "./ServicesContainer";
+import PortfolioContainer from "./PortfolioContainer";
+// import FaqsContainer from "./FaqsContainer"
 
 const Main = () => {
     return (
         <main className={style.main}>
             <FirstContainer />
             <AboutContainer />
-            {/* ServicesContainer */}
-            {/* PortfolioContainer */}
+            <ServicesContainer />
+            <PortfolioContainer />
             {/* FaqsContainer */}
         </main>
     );
