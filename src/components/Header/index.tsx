865bcf6cecbae0f74e508ec6c62eb07ade3ca370
Matvey Vidyasov <kicksukoh@gmail.com>
2025-01-05 13:40:01 +0300

    feat: добавлен компонент Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
 import style from "./Header.module.css"
+import Button from "../Button";
 import ThemeSwitcher from "../ThemeSwitcher";
 
 const Header = () => {
     return (
         <header className={style.header}>
-            HEADER  
-            <ThemeSwitcher />
+            <nav>
+                <div className={style.logo}>МАТВЕЙ</div>
+                <ol>
+                    <li>Home</li>
+                    <li>About Me</li>
+                    <li>Portfolio</li>
+                    <li>Services</li>
+                </ol>
+                <Button>Contact Me</Button>
+                <ThemeSwitcher />
+            </nav>
         </header>
     );
 }
